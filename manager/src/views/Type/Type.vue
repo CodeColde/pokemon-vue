7690bc64503a2ef1ef0ec96ac80e266a56173137
Hayo Friese <hayo@oberon.nl>
2018-11-29 18:09:35 +0100

    Rerouting Types

---
+++ manager/src/views/Type/Type.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="type">
+        <Navigation />
+        <h1>Types</h1>
+        <div class='table-wrap'>
+			<div>
+				<button>Types</button>
+				<button>Type Matrix</button>
+			</div>
+		</div>
+        <div>
+            <div class="typeList">
+            </div>
+            <div class="typeMatrix">
+                <TypeMatrix />
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import Navigation from '@/components/Navigation';
+    import TypeMatrix from '@/components/TypeMatrix';
+    export default{
+        name: 'Type',
+        components: {
+            TypeMatrix
+        },
+        data() {
+            return {
+                currentView: 'Types'
+            }
+        }
+    }
+</script>
